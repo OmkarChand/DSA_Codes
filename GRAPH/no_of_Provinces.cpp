f0f54d7ac987bc62e2022753c2e1539d95585469
OMKAR CHAND DESHLAHRE <111699930+OmkarChand@users.noreply.github.com>
2025-02-05 06:36:04 +0000

    no of provinces

---
+++ GRAPH/no_of_Provinces.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+// Undirected graph
+void addEdges(vector<vector<int>> &adjls, int m) {
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adjls[u].push_back(v);
+        adjls[v].push_back(u);
+    }
+}
+
+void printGraph(vector<vector<int>> adjls, int n, int m) {
+    cout << "Adj list of given Graph:" << endl;
+    for (int i = 1; i <= n; i++) {
+        cout << "Nodes connected to " << i << ": ";
+        for (auto it: adjls[i]) {
+            cout << it << " " ;
+        }
+        cout << endl;
+    }
+}
+
+void dfs(int s, vector<vector<int>> &adjls, vector<int> &vis) {
+    vis[s] = 1;
+    for (auto it : adjls[s]) {
+        if(!vis[it]) {
+            dfs(it, adjls, vis);
+        }
+    }
+}
+
+void noOfProvinces(vector<vector<int>> &adjls, int n) {
+    int cnt = 0;
+    vector<int> vis (n+1, 0);
+    for(int i = 1; i <= n; i++) {
+        if(!vis[i]) {
+            cnt++;
+            dfs(i, adjls, vis);
+        }
+    }
+    cout << "No. of provinces: " << cnt << endl;
+}
+
+int main() {
+    int n, m;
+    cout << "enter no of nodes and edges: ";
+    cin >> n >> m;
+
+    vector<vector<int>> adjls (n+1);
+    cout << "enter edges : " << endl;
+    addEdges(adjls, m);
+
+    printGraph(adjls, n, m);
+
+    noOfProvinces(adjls, n);
+
+    return 0;
+}
