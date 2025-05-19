74079cf867cff8f72014fab683ccd530c3519a04
OMKAR CHAND DESHLAHRE <111699930+OmkarChand@users.noreply.github.com>
2025-05-19 10:34:13 +0000

    2 patterns done

--- Patterns/2.cpp
+++ Patterns/2.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 
-// Rectangle * pattern
+// Right Triangle * pattern
 int main() {
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
             cout << "* ";
         }
         cout << endl;
